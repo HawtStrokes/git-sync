4fc72f2a9d7dbd6657c6fd3210453f3065add6bf
HawtStrokes <hawtstrokes1337@gmail.com>
2025-01-27 15:41:29 +0800

    Add functionality to not automatically push commits

--- git-sync/__main__.py
+++ git-sync/__main__.py
@@ -2,14 +2,22 @@
 from .sync import sync_folder
 from .watcher import monitor_folders
 import json
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Git Sync Project")
+    parser.add_argument('--no-push', action='store_true', help="Disable pushing changes to remote repository")
+    args = parser.parse_args()
+    return args
 
 def main():
+    args = parse_arguments()
+    push_changes = not args.no_push
     # Load configuration
-    with open("config.json", "r") as f:
+    with open('config.json', "r") as f:
         config = json.load(f)
-
     # Start monitoring and syncing folders
-    monitor_folders(config)
+    monitor_folders(config, push_changes=push_changes)
 
 if __name__ == "__main__":
     main()
--- git-sync/sync.py
+++ git-sync/sync.py
@@ -12,7 +12,7 @@ def update_remote_url(repo_path, username, token):
     new_url = f"https://{username}:{token}@github.com/{username}/{repo.remotes.origin.url.split('/')[-1]}"
     origin.set_url(new_url)
 
-def sync_folder(folder_config):
+def sync_folder(folder_config, push_changes=True):
     folder = os.path.expanduser(folder_config["folder_to_sync"])
     repo_url = folder_config["repo_url"]
     branch = folder_config["branch"]
@@ -24,19 +24,23 @@ def sync_folder(folder_config):
     if not os.path.exists(os.path.join(folder, ".git")):
         print(f"Initializing repository at {folder}...")
         Repo.init(folder)
-    
+
     repo = Repo(folder)
 
     # Update the remote URL to use the provided username and token for authentication
     update_remote_url(folder, username, token)
 
-    # Commit and push changes
+    # Commit and (optionally) push changes
     if repo.is_dirty(untracked_files=True):
         repo.git.add(A=True)
         commit_message = commit_message_template.format(
             timestamp=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         )
         repo.index.commit(commit_message)
-        origin = repo.remotes.origin
-        origin.push(branch)
+        if push_changes:
+            origin = repo.remotes.origin
+            origin.push(branch)
+        else:
+            print(f"Changes committed to {folder}, but not pushed to the remote repository.")
+
 
